1af7c9768f3f0c38093b5646459805fc3a26f1b6
Lucas França <lucas.rfranca@gmail.com>
2022-10-20 03:32:32 -0300

    Mostra os números pares de 21 até 30

---
+++ pares_21-30.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Faça um programa em C que mostre na tela os números pares de 21 até 30. */
+
+main() {
+	
+	int i;
+	
+	for (i=22; i<=30; i+=2) {
+		
+		printf ("%d ", i);
+		
+	}
+	
+}
