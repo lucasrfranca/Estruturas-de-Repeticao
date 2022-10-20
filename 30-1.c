d41c982d29ccf0954fc81cd968827ae6304e9bc0
Lucas França <lucas.rfranca@gmail.com>
2022-10-20 03:30:54 -0300

    Mostra os números de 1 até 30 em ordem decrescente

---
+++ 30-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Faça um programa em C que mostre os números de 1 até 30 em ordem decrescente. */
+
+main() {
+	
+	int i;
+	
+	for (i=30; i>=1; i--) { 
+		
+		printf ("%d ", i);
+		
+	}
+	
+}
