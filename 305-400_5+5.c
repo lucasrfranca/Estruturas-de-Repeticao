4a4470340f3c123c5772dfaa96ac4990f9c23933
Lucas França <lucas.rfranca@gmail.com>
2022-10-20 03:19:53 -0300

    Mostra os números de 305 até 400 de 5 em 5

---
+++ 305-400_5+5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Faça um programa em C que mostre os números de 305 até 400 de 5 em 5. */
+
+main() {
+	
+	int i;
+	
+	for (i=305; i<=400; i+=5) {
+		
+		printf ("%d ", i);
+		
+	}
+	
+}
