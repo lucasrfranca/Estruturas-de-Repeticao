2467712f710c3841aeca5c7ea858755a833202a8
Lucas França <lucas.rfranca@gmail.com>
2022-10-20 03:29:56 -0300

    Mostra os números ímpares de 50 até 70

---
+++ impar_50-70.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Faça um programa em C que mostre os números ímpares de 50 até 70.  */
+
+main() {
+	
+	int i;
+	
+	for (i=51; i<=69; i+=2) {
+		
+		printf ("%d ", i);
+		
+	}
+	
+}
