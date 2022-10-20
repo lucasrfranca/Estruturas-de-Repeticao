b73e4780efcbfaf937c168043b76bcae596655f6
Lucas França <lucas.rfranca@gmail.com>
2022-10-20 03:18:07 -0300

    Mostra os números de 100 até 200

---
+++ 100-200.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Faça um programa em C que mostre os números de 100 até 200. */
+
+main() {
+	
+	int i;
+	
+	for (i=100; i<=200; i++) {
+		
+		printf ("%d ", i);
+		
+	}
+	
+}
